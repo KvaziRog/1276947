73a4364aafcb3225bd6203b2f835ceea5c918db9
KvaziRog <61164663+KvaziRog@users.noreply.github.com>
2020-06-08 12:33:24 +0300

    Add files via upload

---
+++ Services.java
@@ -0,0 +1,32 @@
+public class Services {
+
+	Barbershop performance;
+
+	public String getHairCut() {
+
+	}
+
+	/**
+	 * 
+	 * @param n
+	 */
+	public void setHairCut(String n) {
+
+	}
+
+	public void lamination() {
+		// TODO - implement Company.lamination
+		throw new UnsupportedOperationException();
+	}
+
+	public void straightening() {
+		// TODO - implement Company.straightening
+		throw new UnsupportedOperationException();
+	}
+
+	public void wash() {
+		// TODO - implement Company.wash
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Workers.java
@@ -0,0 +1,9 @@
+public class Workers {
+
+	private Stirng nameW;
+	private String position;
+	private Int numberT;
+	private Int chart;
+	private int timeW;
+
+}
